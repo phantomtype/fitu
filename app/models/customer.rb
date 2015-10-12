e2d0f607b7dd7c2c6d5cb4caf04a9d88665b0ec8
YusukeKokubo <yusuke.kokubo@gmail.com>
2015-10-13 00:13:44 +0900

    guard

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,7 @@
 class Customer < ActiveRecord::Base
   def age
+    return nil if self.birth.blank?
+
     d1=self.birth.strftime("%Y%m%d").to_i
     d2=Time.now.strftime("%Y%m%d").to_i
     return (d2 - d1) / 10000
